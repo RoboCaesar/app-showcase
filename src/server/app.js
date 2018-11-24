98b8e0751a707d8df3464aad45a180cc446d3558
Thomas Draxler <twdrax@gmail.com>
2018-11-23 20:37:52 -0800

    Changed build options so webpack builds the client side and server side scripts slightly differently. Index page loads without errors.

--- src/server/app.js
+++ src/server/app.js
@@ -5,7 +5,7 @@ import express from 'express';
 
 const app = express(),
 	    DIST_DIR = __dirname,
-	    HTML_FILE = path.join(DIST_DIR, 'index.html')
+	    HTML_FILE = path.join(DIST_DIR, '../html/index.html')
 app.use(express.static(DIST_DIR));
 
 app.get('/', (req, res) => {

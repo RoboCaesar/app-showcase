8642ca7ea00bb74e88b0bf911047bf70f727d3b4
Thomas Draxler <twdrax@gmail.com>
2018-11-25 16:52:15 -0800

    Added some quick styling.

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Index from './components/Index';
+import '../styles/simple-styles.css';
 
 ReactDOM.render(<Index />, document.getElementById('app'));
